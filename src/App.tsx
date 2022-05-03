54e75d1ed361f25d6c989e1c055fa8ca4cc70408
zekiblue <zekiberkulu@gmail.com>
2022-05-03 17:42:06 +0200

    Add styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <h1> Under Construction! </h1>
+      <h1 className="title"> Temple Delphi </h1>
+        <h2 className="subtitle">
+            Temple Delphi is a oracle where you NFT pricing becomes ease. We are still under development.
+            You can check our api from  <a href="https://api.templedelphi.com">here</a>
+        </h2>
     </div>
   )
 }
